0179e176a57092d43a640b0944d5994e6ef73de4
li-zhang1 <zhangli0623janet@gmail.com>
2023-11-17 07:13:41 -0500

    Implemented image upload feature S11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,7 +47,9 @@ public class AccountController : BaseApiController
     [HttpPost("login")] //api/account/login
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+        var user = await _context.Users
+        .Include(p => p.Photos)
+        .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
         if (user == null) return Unauthorized("Invalid Username");
         using var hmac = new HMACSHA512(user.PasswordSalt);
         var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
@@ -58,7 +60,8 @@ public class AccountController : BaseApiController
         return new UserDto
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain).Url,
         };
 
 
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -16,7 +16,7 @@ export class MembersService {
   getMembers() {
     if (this.members.length > 0) return of(this.members);
     return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map(members=> {
+      map(members => {
         this.members = members;
         return members;
       })
@@ -37,6 +37,15 @@ export class MembersService {
         this.members[index] = { ...this.members[index], ...member };
       })
     )
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+    
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
 
